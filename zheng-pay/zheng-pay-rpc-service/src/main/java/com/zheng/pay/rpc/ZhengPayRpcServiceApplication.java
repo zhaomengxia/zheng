f20734297701c2e4dd33048b623bd0ab4f8e037f
zhaomengxia <zhaomengxia@vortexinfo.cn>
2019-02-18 15:08:37 +0800

    lalala

---
+++ zheng-pay/zheng-pay-rpc-service/src/main/java/com/zheng/pay/rpc/ZhengPayRpcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.pay.rpc;
+
+/**
+ * 服务启动类
+ * Created by ZhangShuzheng on 2017/3/29.
+ */
+public class ZhengPayRpcServiceApplication {
+
+	public static void main(String[] args) {
+		com.alibaba.dubbo.container.Main.main(args);
+	}
+
+}
---
+++ zheng-upms/zheng-upms-rpc-service/src/main/java/com/zheng/upms/rpc/ZhengUpmsRpcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.upms.rpc;
+
+/**
+ * 服务启动类
+ * Created by ZhangShuzheng on 2017/2/3.
+ */
+public class ZhengUpmsRpcServiceApplication {
+
+	public static void main(String[] args) {
+		com.alibaba.dubbo.container.Main.main(args);
+	}
+
+}
