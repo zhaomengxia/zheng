55c4ee20b1108ab680ea67160974b65497ab715c
zhaomengxia <zhaomengxia@vortexinfo.cn>
2019-02-18 15:03:54 +0800

    lala

---
+++ zheng-api/zheng-api-rpc-service/src/main/java/com/zheng/api/rpc/ZhengApiRpcServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zheng.api.rpc;
+
+/**
+ * 服务启动类
+ * Created by shuzheng on 2017/2/19.
+ */
+public class ZhengApiRpcServiceApplication {
+
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
---
+++ zheng-cms/zheng-cms-rpc-service/src/main/java/com/zheng/cms/rpc/ZhengCmsRpcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.cms.rpc;
+
+/**
+ * 服务启动类
+ * Created by ZhangShuzheng on 2017/2/3.
+ */
+public class ZhengCmsRpcServiceApplication {
+
+	public static void main(String[] args) {
+		com.alibaba.dubbo.container.Main.main(args);
+	}
+
+}
